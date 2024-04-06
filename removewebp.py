1ca3f404b48a29c7c5e74afa8104f840a6203063
Mitja Rislakki <57709659+mitjarislakki@users.noreply.github.com>
2024-04-06 18:52:41 +0300

    remove redundant tools

--- removewebp.py
+++
@@ -1,7 +0,0 @@
-import os
-
-input_directory = 'imageSet/'
-
-for filename in os.listdir(input_directory):
-        if filename.endswith('.webp'):
-                os.remove(os.path.join(input_directory, filename))
